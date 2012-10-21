17b7a2eeb47c5f4540d94d7ad780d67c6f934cce
Christopher Childs <cjchilds@me.com>
2012-10-20 23:39:56 -0400

    Allow the user to specify a .s3cfg file.
    Done in order to make deployment a bit less of a hassle.

--- build/deploy.py
+++ build/deploy.py
@@ -3,13 +3,20 @@
 from fabricate import *
 import shutil
 import os
+import sys
 import subprocess
 
 HOME_DIR = os.getcwd()
 
+if 'S3CONFIG' not in os.environ:
+    print 'S3CONFIG not defined'
+    S3CONFIG='./.s3cfg'
+else:
+    S3CONFIG=os.environ['S3CONFIG']
+
 def s3deploy():
     os.chdir('./target/site')
-    subprocess.call(['s3cmd', 'sync', '.', 's3://hubspoke.github.com'])
+    subprocess.call(['s3cmd', '--config='+S3CONFIG, 'sync', '.', 's3://hubspoke.github.com'])
     os.chdir(HOME_DIR)
 
 def build():
