a4b21809495059b32ea685c7f27531a1b6f12f6b
Christopher Childs <cjchilds@me.com>
2012-10-21 00:19:44 -0400

    Cleanup the target S3 bucket before syncing.

--- build/deploy.py
+++ build/deploy.py
@@ -5,6 +5,7 @@ import shutil
 import os
 import sys
 import subprocess
+import boto
 
 HOME_DIR = os.getcwd()
 
@@ -14,7 +15,16 @@ if 'S3CONFIG' not in os.environ:
 else:
     S3CONFIG=os.environ['S3CONFIG']
 
+def cleanup_s3():
+    s3 = boto.connect_s3()
+    b = s3.get_bucket('hubspoke.github.com')
+    keys = b.list()
+    for k in keys:
+        if not os.path.exists('./target/site/' + k.key):
+            k.delete()
+
 def s3deploy():
+    cleanup_s3()
     os.chdir('./target/site')
     subprocess.call(['s3cmd', '--config='+S3CONFIG, 'sync', '.', 's3://hubspoke.github.com'])
     os.chdir(HOME_DIR)
